e722cd2cc44bd7d25e87a4b2d00bf4079806dbeb
Satyaki Saha <79146351+satyaki95@users.noreply.github.com>
2023-04-07 14:37:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your JS code here. If required.
+const form = document.getElementById('form');
+
+form.addEventListener('submit',handelSubmit);
+
+function handelSubmit(event){
+	const first = document.getElementById('first');
+	const last = document.getElementById('last');
+	const phone = document.getElementById('phone');
+	const email = document.getElementById('email');
+
+	    window.alert(`
+    First Name: ${first.value}
+    Last Name: ${last.value}
+    Phone Number: ${phone.value}
+    Email ID: ${email.value}`);
+
+	event.preventDefault();
+  }
